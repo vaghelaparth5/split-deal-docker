0c604088606d414e6a8eec6376b9a90cfa08f11a
JosephKS10 <jjbrothersdelhi@hotmail.com>
2025-05-09 23:12:09 +1000

    Create /createGroup endpoint

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -1,3 +1,44 @@
+const Group = require("../models/Group");
+
+// Create a new group
+exports.createGroup = async (req, res) => {
+  try {
+    const {
+      dealId,
+      dealLogo,
+      dealTitle,
+      dealDescription,
+      storeName,
+      storeLocation,
+      totalValue,
+      discount,
+      expiryDate,
+      membersRequired,
+      receiptImage,
+    } = req.body;
+
+    // Create and save group
+    const group = new Group({
+        dealId,
+      dealLogo,
+      dealTitle,
+      dealDescription,
+      storeName,
+      storeLocation,
+      totalValue,
+      discount,
+      expiryDate: new Date(expiryDate), // Ensure it's saved as a Date object
+      membersRequired,
+      receiptImage,
+    });
+    await group.save();
+
+    res.status(201).json({ msg: "Group created successfully", group });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error", error });
+  }
+};
+
 exports.updateGroupStatus = async (req, res) => {
   try {
     const { id } = req.params;
--- src/models/groupRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {updateGroupStatus} = require("../controllers/groupController");
-
-router.put("/update-group-status/:id",authMiddleware, updateGroupStatus);
