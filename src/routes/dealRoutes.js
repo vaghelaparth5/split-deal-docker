6d13ba1c529c5a411c900c4c8145c234d85558f1
joshua <s225208726@deakin.edu.au>
2025-05-12 22:31:21 +1000

    dealExpiry1

--- src/routes/dealRoutes.js
+++ src/routes/dealRoutes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {
   createDeal,
-
+  expireDeals
 } = require("../controllers/dealController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Create a new deal
 router.post("/create", authMiddleware, createDeal);
+
+// Manually expire outdated deals
+router.patch("/expire", authMiddleware, expireDeals);
 
 module.exports = router;
