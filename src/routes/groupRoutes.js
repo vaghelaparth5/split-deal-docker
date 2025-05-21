82ad75392ca3156433cfdb2838c77d509f6b1a07
225108954 <s225108954@deakin.edu.au>
2025-05-21 12:13:55 +1000

    Implement getAllGroups (Controller + Route)

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -8,5 +8,9 @@ const authMiddleware = require("../middleware/authMiddleware");
 //  Route to create a new group
 router.post("/create-group",authMiddleware, createGroup);
 
+//  Route to get all groups
+router.get("/get-groups", getAllGroups);
+
+
 //  Route to update group status
 router.put("/update-group-status/:id",authMiddleware, updateGroupStatus);
